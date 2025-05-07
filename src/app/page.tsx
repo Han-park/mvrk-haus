7c072e00b6c6b178b861ac733dd5f0988ec0ae3b
Han Park <60293425+Han-park@users.noreply.github.com>
2025-05-08 03:13:15 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,16 @@ export default function Home() {
       <footer className="pb-30 bg-black">
         <div className="container mx-auto px-4 text-center font-roboto-mono text-sm">
           <p className="text-gray-400">Website coming soon.</p>
-          <p className="text-gray-600">by MVRK CRAFT from MVRK HAUS.</p>
+          <p className="text-gray-600">
+            by{" "}
+            <a
+              href="mailto:me@han-park.info?subject=메버릭 크래프트가 궁금해요&body=메버릭 동료이신가요? 메버릭 크래프트에 대해 궁금한 점을 적어주세요. 메버릭 하우스 내/외부의 연결을 기술을 통해 강화하는 것이 저희의 미션이며, 무언가를 손으로 만드는 걸 좋아하는 사람들이 모여있는 집단이에요. - 종한 드림"
+              className="text-gray-600 hover:text-gray-400"
+            >
+              MVRK CRAFT
+            </a>{" "}
+            from MVRK HAUS.
+          </p>
         </div>
       </footer>
     </div>
